cd7f39c919f04bf80148c7c74ef7733d17a53ba8
s-a-malik <shreshth_malik@yahoo.co.uk>
2022-04-24 01:00:34 +0100

    add the server api

--- server/carbon_density.py
+++ server/carbon_density.py
@@ -51,12 +51,18 @@ def read_image_from_url(url):
     return rasterio.open("example1.tif")
 
 
-def get_satellite_data(verra_data):
+def get_carbon_data(verra_data):
     """Gets the biomass at a location and time from satellite imagery
     """
+    est_annual_redct = 0
+    for i in verra_data["participationSummaries"][0]["attributes"]:
+        if i["code"] == "EST_ANNUAL_EMISSION_REDCT":
+            print(i)
+            est_annual_redct = int(i["values"][0]["value"])
+    print("est annual redct", est_annual_redct)
+
     location = verra_data["location"]
     print("location", location)
-
     lat_c = location["latitude"]
     lon_c = location["longitude"]
 
@@ -67,9 +73,9 @@ def get_satellite_data(verra_data):
     print("lat tl", lat_tl, "long tl", lon_tl)
 
     biomass = {}    # biomass for different times
+    datasets = {}
 
     for year in [2010, 2017, 2018]:
-    # for year in [2010]:
         print(f"{DCLIMATE_URL}/{year}/{lat_tl}_{lon_tl}/AGB")
         dataset = read_image_from_url(f"{DCLIMATE_URL}/{year}/{lat_tl}_{lon_tl}/AGB") 
         # dataset = rasterio.open("example1.tif")
@@ -77,39 +83,41 @@ def get_satellite_data(verra_data):
         print(dataset.count, dataset.height, dataset.width, dataset.crs, dataset.bounds)
         print(dataset.read(1))
         row, col = dataset.index(lat_c, lon_c)
-        print("centre point of matrix", row, col)
-        print("bottom right", dataset.index(dataset.bounds.bottom, dataset.bounds.right))
-        
-        biomass[year] = calculate_carbon_density(verra_data, dataset)
+        # print("centre point of matrix", row, col)
+        # print("bottom right", dataset.index(dataset.bounds.bottom, dataset.bounds.right))
+        biomass[year], indices = calculate_carbon_density(verra_data, dataset)
+        datasets[year] = dataset
 
-        # show(dataset)
-    print(biomass)
+    print("biomass data", biomass)
 
     baseline = np.mean([biomass[2010], biomass[2017]])
     print(f"Baseline: {baseline}")
     delta = biomass[2018] - baseline
     print("delta", delta)
 
-    # if delta < 
-    #     # mint no token
+    # get the bands
+    band_2010 = datasets[2010].read(1)
+    band_2017 = datasets[2017].read(1)
+    band_2018 = datasets[2017].read(1)
+    baseline_band = np.mean( np.array([band_2010, band_2017]), axis=0 )
+    delta_band = band_2018 - baseline_band
 
-    # else:
-    #     # mint yes token
+    # plot the delta
+    # show(delta_band[indices[0]:indices[1], indices[2]:indices[3]])
 
-    # # return delta 
+    carbon_data = {
+        "baselineBiomass5Year": baseline,
+        "satelliteDelta1Year": delta,
+        "projectedDelta1Year": est_annual_redct
+    }
 
-
+    return carbon_data
 
 
 def calculate_carbon_density(verra_data, satellite_data):
 
-    est_annual_redct = 0
     size_in_sqkm = 0
     for i in verra_data["participationSummaries"][0]["attributes"]:
-        if i["code"] == "EST_ANNUAL_EMISSION_REDCT":
-            print(i)
-            est_annual_redct = int(i["values"][0]["value"])
-
         if i["code"] == "PROJECT_ACREAGE":
             size_str = i["values"][0]["value"]
             # check if hectares or acres and convert to km
@@ -120,7 +128,8 @@ def calculate_carbon_density(verra_data, satellite_data):
             else:
                 print("Could not find size of project")
                 return 0
-    print("est annual redct", est_annual_redct, "size in sq km", size_in_sqkm)
+    
+    # print("size in sq km", size_in_sqkm)
     length_of_square_km = np.sqrt(size_in_sqkm)
     print("box length", length_of_square_km)
     r = length_of_square_km / 2
@@ -139,7 +148,7 @@ def calculate_carbon_density(verra_data, satellite_data):
     max_lat = f + df
     min_lon = l - dl
     max_lon = l + dl
-    # print("min lat", min_lat, "max lat", max_lat, "min lon", min_lon, "max lon", max_lon)
+    print("min lat", min_lat, "max lat", max_lat, "min lon", min_lon, "max lon", max_lon)
 
     band1 = satellite_data.read(1)
     # print(np.shape(band1))
@@ -159,15 +168,15 @@ def calculate_carbon_density(verra_data, satellite_data):
 
 
     # print(min_row, min_col, max_row, max_col)
-    show(band1[min_row:max_row, min_col:max_col])
+    # show(band1[min_row:max_row, min_col:max_col])
     carbon_density_total = np.sum(band1[min_row:max_row, min_col:max_col])
     # print(carbon_density_total)
 
-    return carbon_density_total
+    return carbon_density_total, (min_row, max_row, min_col, max_col)
 
 
 
-def carbon_density(project_id):
+def get_carbon_density(project_id):
     """
     Returns the carbon density of a verra project given its ID
     Params:
@@ -176,48 +185,16 @@ def carbon_density(project_id):
     Returns:
     - carbon (float): The carbon density of the project
     """
-    carbon = 0
     
     verra_data = get_verra_data(project_id)
-    satellite_data = get_satellite_data(location)
-    calculate_carbon_density(verra_data, satellite_data)
+    carbon_data = get_carbon_data(verra_data)
 
-    return carbon
+    return carbon_data
 
 
 if __name__ == "__main__":
-    # print(carbon_density(3018, "2010-01-01"))
 
-    # fp = 'example.tif'
-    # dataset = rasterio.open(fp)
-    # No. of Bands and Image resolution
-    # print(dataset.count, dataset.height, dataset.width, dataset.crs, dataset.bounds)
-    # Coordinate Reference System
-    
-    # show(img)
-    verra_data = get_verra_data(1542)
-    # verra id 1911
-    # delta -585
-    # baseline 366043
-    # {2010: 378038, 2017: 354048, 2018: 365458}
-    #     "location": {
-    #     "latitude": 47.470000,
-    #     "longitude": -121.840000
-    # },
-    # "value": "815 Acres"
+    get_carbon_data(607)
 
-
-    # 607
-    #     "location": {
-    #     "latitude": 49.288547,
-    #     "longitude": -116.846603
-    # },
-
-    # 1542 on toucan
-    # location {'latitude': 25.908866, 'longitude': 102.968453}
-    #    est annual redct 54734 size in sq km 68.79
-    # box length 8.293973715897586
-    # {2010: 231486, 2017: 365191, 2018: 388331}
-    # Baseline: 298338.5
-    # delta 89992.5
-    get_satellite_data(verra_data)
+    # verra_data = get_verra_data(607)
+    # get_satellite_data(verra_data)
